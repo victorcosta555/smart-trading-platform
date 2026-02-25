1107249a188f518df211edd793edfc39e8e78024
costavictor007 <victor.costa@fromjimmy.com>
2026-02-25 18:37:27 +0100

    Initialize auth-service project

---
+++ services/auth-service/src/test/java/com/smarttrading/platform/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttrading.platform.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
